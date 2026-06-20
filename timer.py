6bf69223062305497d4f9d1df37e787465ebe38b
angelalucban07-bit <angelalucban07@gmail.com>
2026-06-20 22:12:26 +0800

    fix: found error and fix formatting.

--- timer.py
+++ timer.py
@@ -6,7 +6,7 @@ class Timer(BaseWindow):
         '''Update timer in minute and second'''
         current_time = time.time()
 
-        if int(current_time - s_time) >= 0):
+        if int(current_time - s_time) >= 0:
             self.seconds += 1
 
         if self.seconds == 60:
@@ -16,7 +16,10 @@ class Timer(BaseWindow):
         min_p = '{:0>2d}'.format(int(self.minutes))
         sec_p = '{:0>2d}'.format(int(self.seconds))
 
-        time_count.config(text=f'{min_p}:{sec_p}')
+        time_count.config(
+            text=f'{min_p}:{sec_p}'
+        )
+
         time_count.after(
         1000,
         lambda: self.update_timer(s_time)
@@ -25,8 +28,13 @@ class Timer(BaseWindow):
     def formatted_time(self, total_time) -> str:
         '''Formating total time in minutes and seconds'''
         time_format = ""
-        time_format = '{:0>2d}'.format(int(total_time / 60))
-        time_format += ':' + '{:0>2d}'.format(int(total_time % 60))
+
+        time_format = '{:0>2d}'.format(
+            int(total_time / 60)
+        )
+        time_format += ':' + '{:0>2d}'.format(
+            int(total_time % 60)
+        )
         return time_format
 
     def get_time(self):
@@ -35,5 +43,4 @@ class Timer(BaseWindow):
 
     def get_total_seconds(self):
         """Return total elapsed seconds"""
-        if self.running:
         return int(self.end_time - self.start_time)
