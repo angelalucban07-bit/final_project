ad6919654f44b776899e7aa04fe0799b1da108b3
angelalucban07-bit <angelalucban07@gmail.com>
2026-06-20 22:31:48 +0800

    refractor: clean up inheritance chain.

--- statistics.py
+++ statistics.py
@@ -3,18 +3,35 @@ from timer import Timer
 class Statistics(Timer):
     def calculate_result(self) -> tuple:
         '''Calculating accuracy, actual accuracy, word per minute(wpm) and total time taken to type paragraph'''
-        total_time = int(self.end_time - self.start_time)
 
-        wpm = correct_letters = correct_words = accuracy = actual_accuracy = 0
+        total_time = int(
+            self.end_time - self.start_time
+        )
+
+        if total_time == 0:
+            total_time = 1
+
+            wpm = 0
+            correct_letters = 0
+            correct_words = 0
+            accuracy = 0
+            actual_accuracy = 0
 
         correct_flag = 1
-        for para_char, user_char in zip(self.paragraph, self.get_user_text):
+        for para_char, user_char in zip(
+                self.paragraph,
+                self.get_user_text
+        ):
             if para_char == user_char:
                 correct_letters += 1
             else:
                 correct_flag = 0
 
-            if para_char == ' ' or para_char == '\n' or para_char == '\t':
+            if (
+                para_char == ' '
+                or para_char == '\n'
+                or para_char == '\t'
+            ):
                 correct_words += 1 if correct_flag == 1 else 0
                 correct_flag = 1
 
@@ -24,7 +41,12 @@ class Statistics(Timer):
         accuracy = (correct_letters * 100) / len(self.paragraph)
         actual_accuracy = (correct_letters - self.backspace_count) * 100 / len(self.paragraph)
 
-        return int(accuracy), int(actual_accuracy), int(wpm), self.formatted_time(total_time)
+        return (
+            int(accuracy),
+            int(actual_accuracy),
+            int(wpm),
+            self.formatted_time(total_time)
+        )
 
     def reset_data(self) -> None:
         self.start_flag = 0
