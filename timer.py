dca2eeff9420d938a770e7929f0601fb30f7fc2b
angelalucban07-bit <angelalucban07@gmail.com>
2026-06-20 21:21:59 +0800

    refractor: seperated timer class to a new module. status: not running.

--- timer.py
+++ timer.py
@@ -1,13 +1,34 @@
 class Timer:
-    def __init__(self):
-        self.__start_time = 0
-        self.__end_time = 0
+    def update_timer(self, s_time) -> None:
+        '''Update timer in minute and second'''
+        current_time = time.time()
 
-    def start(self):
-        self.__start_time = time.time()
+        if (int(current_time - s_time) >= 0):
+            self.seconds += 1
+        if self.seconds == 60:
+            self.seconds = 0
+            self.minutes += 1
 
-    def stop(self):
-        self.__end_time = time.time()
+        min_p = '{:0>2d}'.format(int(self.minutes))
+        sec_p = '{:0>2d}'.format(int(self.seconds))
 
-    def get_elapsed_time(self):
-        return int(self.__end_time - self.__start_time)
+        time_count.config(text=f'{min_p}:{sec_p}')
+        time_count.after(1000, lambda: self.update_timer(s_time))
+
+    def formatted_time(self, total_time) -> str:
+        '''Formating total time in minutes and seconds'''
+        time_format = ""
+        time_format = '{:0>2d}'.format(int(total_time / 60))
+        time_format += ':' + '{:0>2d}'.format(int(total_time % 60))
+        return time_format
+
+    def get_time(self):
+        """Return current time as mm:ss"""
+        self.update()
+        return f"{self.minutes:02}:{self.seconds:02}"
+
+    def get_total_seconds(self):
+        """Return total elapsed seconds"""
+        if self.running:
+            return int(time.time() - self.start_time)
+        return int(self.end_time - self.start_time)
